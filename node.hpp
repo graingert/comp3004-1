565f66ff21e73aae7783b57dd4dd9b3ee1bb2a36
Thomas Grainger <tagrain@gmail.com>
2012-01-10 10:41:24 +0000

    Added node from http://scriptionary.com/2009/02/17/simple-scene-graph-in-c/

---
+++ node.hpp
@@ -0,0 +1,34 @@
+//http://scriptionary.com/2009/02/17/simple-scene-graph-in-c/
+
+#ifndef __node_h_
+#define __node_h_ 1
+#include <vector>
+ 
+class Node
+{
+public:
+    Node(Node* Parent = NULL, const char* Name = NULL);
+    virtual ~Node(void);
+ 
+    virtual void Update(void);
+ 
+    Node* GetParentNode(void) const;
+    void SetParentNode(Node* NewParent);
+ 
+    void AddChildNode(Node* ChildNode);
+    void RemoveChildNode(Node* ChildNode);
+ 
+    const char* GetNodeName(void) const;
+    const size_t CountChildNodes(const bool& RecursiveCount = false) const;
+    virtual const bool IsRootNode(void) const = 0;
+ 
+    Node* GetChildNodeByName(const char* SearchName);
+ 
+private:
+    Node* m_Parent;
+    const char* m_Name;
+    std::vector<Node*> m_Children;
+ 
+}; // class Node
+ 
+#endif
